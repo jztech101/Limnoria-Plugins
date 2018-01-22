90a4a67ce08074e813cb297858440c2f15405e9a
jztech101 <jzhou2oo6@gmail.com>
2018-01-22 18:41:55 +0000

    Fixed Logging Formatting

--- plugin.py
+++ plugin.py
@@ -21,7 +21,10 @@ class Loggy(callbacks.Plugin):
         logChannel = self.registryValue('LogChan')
         if logChannel is None or not logChannel.startswith("#"):
             return
-        irc.queueMsg(ircmsgs.privmsg(logChannel, "[Notice " + msg.args[0] +"] " + msg.prefix + ': '  +' '.join(msg.args[1:])))
+        if msg.args[0].startswith('#'):
+            irc.queueMsg(ircmsgs.privmsg(logChannel, "[Notice] " + msg.prefix + ': (' +msg.args[0]+ ') '  +' '.join(msg.args[1:])))
+        else:
+            irc.queueMsg(ircmsgs.privmsg(logChannel, "[Notice] " + msg.prefix + ': '  +' '.join(msg.args[1:])))
     def doPrivmsg(self,irc,msg):
         logChannel = self.registryValue('LogChan')
         if logChannel is None or msg.args[0].startswith("#") or not logChannel.startswith("#"):
