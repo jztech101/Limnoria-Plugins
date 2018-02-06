f5a435abfdf2c020aea554594b22ec8ce1897f02
Julian Zhou <jzhou2oo6@gmail.com>
2018-02-06 05:50:44 +0000

    Mass Highlight Spam Detection

--- RandKicks/plugin.py
+++ RandKicks/plugin.py
@@ -29,21 +29,32 @@ class RandKicks(callbacks.Plugin):
         funregexes = []
         funkickmsg = []
         spamregexes.append('.*▄.*▄.*')
-        spamkickmsg.append('Spam')
+        spamkickmsg.append('Spam Script')
         #spamregexes.append('test2')
         #spamkickmsg.append('Spam2')
         funregexes.append('.*i.*l.*o.*v.*e.*W.*i.*l.*l.*y.*o.*u.*m.*e.*')
         funkickmsg.append("No")
         #funregexes.append('test3')
         #funkickmsg.append('Testing2')
+        nicks = 0
         if SpamDet:
             for i in range(0, len(spamregexes)):
                 if re.match(spamregexes[i],' '.join(msg.args[1:]), re.IGNORECASE):
-                    irc.queueMsg(ircmsgs.kick(msg.args[0],msg.nick,spamkickmsg[i])) 
+                    irc.queueMsg(ircmsgs.kick(msg.args[0],msg.nick,spamkickmsg[i]))
+                    return
+            for i in str(msg.args[1:]).split(" "):
+                i = re.sub(r'\W+','',i)
+                if i in irc.state.channels[msg.args[0]].users:
+                    nicks = nicks + 1
+                if nicks >= 3:
+                    irc.queueMsg(ircmsgs.kick(msg.args[0],msg.nick,"Mass Highlight Spam"))
+                    return
+
         if FunDet:
             for i in range(0, len(funregexes)):
                 if re.match(funregexes[i],' '.join(msg.args[1:]), re.IGNORECASE):
                     irc.queueMsg(ircmsgs.kick(msg.args[0],msg.nick,funkickmsg[i]))
+                    return
 Class = RandKicks
 
 
