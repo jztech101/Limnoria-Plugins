1832e7c794a4491718a704412be17b1be2da78b4
jztech101 <jzhou2oo6@gmail.com>
2018-04-29 16:59:56 +0000

    Change delay to 60 seconds

--- TimeBomb/plugin.py
+++ TimeBomb/plugin.py
@@ -58,9 +58,9 @@ class TimeBomb(callbacks.Plugin):
         self.chan = msg.args[0]
         if logChannel:
             irc.queueMsg(ircmsgs.privmsg(logChannel, "[TimeBomb] " + msg.prefix + ': (' + self.chan + ') A bomb has been shoved down ' + self.bombtarget + '\'s underwear, the good wire is the ' + self.goodWire + ' one'))
-        irc.reply('A bomb has been shoved inside ' + self.bombtarget + '\'s underwear, it will detonate in 30 seconds. These are the wires: ' + ' '.join(self.wires))
+        irc.reply('A bomb has been shoved inside ' + self.bombtarget + '\'s underwear, it will detonate in 60 seconds. These are the wires: ' + ' '.join(self.wires))
         #schedule.removeEvent('detonate')
-        schedule.addEvent(self.detonate, time.time() + 30, 'detonate', [irc])
+        schedule.addEvent(self.detonate, time.time() + 60, 'detonate', [irc])
     timebomb = wrap(timebomb, [optional(many('something'))])
     def cut(self, irc, msg, args, something):
         """cut"""
