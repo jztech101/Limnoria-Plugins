e788a650085d1d42c60a76fc4596fd3c371d4159
Julian Zhou <jzhou2oo6@gmail.com>
2018-08-14 21:39:00 +0000

    Fix mitigation

--- RandKicks/plugin.py
+++ RandKicks/plugin.py
@@ -45,7 +45,7 @@ class RandKicks(callbacks.Plugin):
         spamkickmsg.append('Spam propoganda')
         spamregexes.append('   ')
         spamkickmsg.append('Spam Spaces')
-        spamregexes.append('   ­ ­  ')
+        spamregexes.append('  ­ ­  ')
         spamkickmsg.append('Spam Spaces')
         funregexes.append('something')
         funkickmsg.append("something else")
